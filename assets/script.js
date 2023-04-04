a8642f77614081d597af9c267c4b17545dd76e09
antoninodangelo11 <reapercsp@gmail.com>
2023-04-04 14:57:21 +0200

    end exercise

---
+++ assets/script.js
@@ -0,0 +1,6 @@
+const userName = prompt('Inserisci il tuo nome');
+const userLastName = prompt('Inserisci il tuo cognome');
+const userColor = prompt('Inserisci il tuo color preferito');
+
+
+document.getElementById('result').innerHTML = (userName + userLastName + userColor + '23');
