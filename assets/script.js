50c8ab2f8c05e4172b06091a95e22220670f22fc
antoninodangelo11 <reapercsp@gmail.com>
2023-04-05 10:05:07 +0200

    correction

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,7 @@
 const userName = prompt('Inserisci il tuo nome');
 const userLastName = prompt('Inserisci il tuo cognome');
-const userColor = prompt('Inserisci il tuo color preferito');
+const userColor = prompt('Inserisci il tuo colore preferito');
 
+const password = userName + userLastName + userColor + '23';
 
-document.getElementById('result').innerHTML = (userName + userLastName + userColor + '23');
+document.getElementById('password').innerHTML = password;
